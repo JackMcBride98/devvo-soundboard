01619e72a588062827ad414f94233b4143c8dbe6
Jack McBride <mcbride.jack1@gmail.com>
2022-02-20 20:43:06 +0000

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import devvoImgUrl from "./images/mc-devvo.jpeg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-col mx-auto max-w-xl">
+      {/* <h1 className="text-2xl font-semi-bold p-4">MC Devvo Soundboard</h1> */}
+      <img src={devvoImgUrl} alt={"mc devvo"} />
     </div>
   );
 }
