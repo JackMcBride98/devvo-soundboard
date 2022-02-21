3fb45c1d8c3f2c03c0b7809b482ca1e82d823b0e
Jack McBride <mcbride.jack1@gmail.com>
2022-02-21 13:43:58 +0000

    Finish app, colour scheme, file download, responsive design, original video

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "huge": "box-shadow: 0 100px 200px -48px rgba(0, 0, 0,  0.5)",
+        "3xl": "box-shadow: 0 35px 60px -15px rgba(0, 0, 0, 0.3)",
+      },
+    },
   },
   plugins: [],
 };
